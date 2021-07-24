70fbbc99d3e26157471a54089f5b0a6b66414394
tohosaku <tohosaku@users.osdn.me>
2021-07-24 15:54:03 +0900

    export function

--- app/assets/javascripts/index.ts
+++ app/assets/javascripts/index.ts
@@ -3,20 +3,18 @@ import { runApp } from "./standalone";
 
 const layoutOptions = {
   "elk.algorithm": "layered",
-  "elk.direction": "DOWN",
+  "elk.direction": "RIGHT",
   "spacing.nodeNode": 60,
   "spacing.nodeNodeBetweenLayers": 60,
   "spacing.edgeNodeBetweenLayers": 30,
 };
 
-export default function(url: string) {
-  import(url).then( workflow => {
-    const graph = {
-      id: "root",
-      layoutOptions,
-      ...workflow
-    };
+export function setGraph(workflow: any) {
+  const graph = {
+    id: "root",
+    layoutOptions,
+    ...workflow
+  }
 
-    runApp("sprotty", graph);
-  })  
+  runApp("sprotty", graph);
 }
