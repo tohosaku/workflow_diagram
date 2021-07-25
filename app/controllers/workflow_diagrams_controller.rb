5bb477a64d48c85a2b8d498b40c4d365a395a786
tohosaku <tohosaku@users.osdn.me>
2021-07-25 10:25:50 +0900

    Reflect changes in the select box.

--- app/controllers/workflow_diagrams_controller.rb
+++ app/controllers/workflow_diagrams_controller.rb
@@ -1,20 +1,21 @@
 class WorkflowDiagramsController < ApplicationController
   # layout 'admin'
   # before_action :require_admin
+  helper :workflows
 
   def show
     respond_to do |format|
       format.html
-      format.json { render json: graph }
+      format.json { render json: graph(params[:tracker_id], params[:role_id]) }
     end
   end
 
   private
 
-  def graph
+  def graph(tracker_id, role_id)
     {
       children: graph_children.map { |c| { id: "n#{c.id}", labels: [{ text: c.name }] } },
-      edges: graph_edges.map do |e|
+      edges: graph_edges(tracker_id, role_id).map do |e|
         { id: "e#{e.id}", sources: ["n#{e.old_status_id}"], targets: ["n#{e.new_status_id}"] }
       end.uniq
     }
@@ -24,7 +25,7 @@ class WorkflowDiagramsController < ApplicationController
     IssueStatus.all
   end
 
-  def graph_edges
-    WorkflowTransition.all
+  def graph_edges(tracker_id, role_id)
+    WorkflowTransition.where(tracker_id: tracker_id, role_id: role_id)
   end
 end
