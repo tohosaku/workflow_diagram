68e6ef613fb34f5d7d3e4d489e51064f55982851
tohosaku <tohosaku@users.osdn.me>
2021-07-25 20:59:28 +0900

    Enabled to identify bidirectional edges.

--- app/controllers/workflow_diagrams_controller.rb
+++ app/controllers/workflow_diagrams_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class WorkflowDiagramsController < ApplicationController
   # layout 'admin'
   # before_action :require_admin
@@ -17,7 +19,7 @@ class WorkflowDiagramsController < ApplicationController
       children: graph_children.map { |c| { id: "n#{c.id}", labels: [{ text: c.name }] } },
       edges: graph_edges(tracker_id, role_id).map do |e|
         { id: "e#{e.id}", sources: ["n#{e.old_status_id}"], targets: ["n#{e.new_status_id}"] }
-      end.uniq
+      end
     }
   end
 
@@ -25,7 +27,34 @@ class WorkflowDiagramsController < ApplicationController
     IssueStatus.all
   end
 
+  SQL = <<~EOSQL
+    SELECT
+        W1.id
+      , W1.old_status_id
+      , W1.new_status_id
+      , W1.assignee
+      , W1.author
+      , W1.tracker_id
+      , W1.role_id
+      , CASE WHEN W2.id IS NOT NULL THEN 'bidir' ELSE 'unidir' END AS direction
+     FROM
+          workflows W1
+          LEFT OUTER JOIN workflows W2 ON
+                         W2.type          = W1.type
+                     AND W2.new_status_id = W1.old_status_id
+                     AND W2.old_status_id = W1.new_status_id
+                     AND W2.tracker_id    = W1.tracker_id
+                     AND W2.role_id       = W1.role_id
+                     AND W2.assignee      = W1.assignee
+                     AND W2.author        = W1.author
+     WHERE
+           W1.type='WorkflowTransition'
+       AND CASE WHEN W2.id IS NOT NULL THEN W1.new_status_id > W1.old_status_id ELSE W1.id = W1.id END
+       AND W1.tracker_id IN (?)
+       AND W1.role_id IN (?)
+  EOSQL
+
   def graph_edges(tracker_id, role_id)
-    WorkflowTransition.where(tracker_id: tracker_id, role_id: role_id)
+    WorkflowTransition.find_by_sql [SQL, [tracker_id], [role_id]]
   end
 end
